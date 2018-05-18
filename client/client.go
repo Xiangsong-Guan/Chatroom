418cdba8a324952dc9b0c5d42ecdad3bd3cdf221
caocao <caocao@gmail.com>
2018-05-18 14:31:46 +0800

    client initial commit

--- client/client.go
+++ client/client.go
@@ -1,9 +1,37 @@
 // Package client 提供了对客户端的实验，并且提供了一些工具
 package client
 
+import (
+	"Chatroom/hub"
+	"Chatroom/message"
+
+	"github.com/gorilla/websocket"
+)
+
 // Client 对应着每一个参与聊天的远程实体，它记录了该实体的信息并且负责
 // 针对该实体收发消息
 type Client struct {
+	send   chan []byte
+	hub    *hub.Hub
+	webCnn *websocket.Conn
+	info   message.Info
+}
+
+// New generate a Client
+func New(hubs *hub.Hub, webConn *websocket.Conn, inform message.Info) Client {
+	return Client{
+		hub:    hubs,
+		webCnn: webConn,
+		info:   inform,
+	}
+}
+
+// readPump 函数
+func (client *Client) ReadPump() {
+}
+
+// writePump 函数
+func (client *Client) WritePump() {
 }
 
 // Sender 能够提供一个可发送的信道来接受消息
--- message/message.go
+++ message/message.go
@@ -1,12 +1,34 @@
 // Package message 实现了对消息的封装，提供了一些对应的工具
 package message
 
+import "time"
+
 // Info 对应一个发送消息的实体，它封装了该实体的信息
 type Info struct {
+	Name      string
+	IPAddress string
+	Mail      string
+}
+
+// GenSummary 实现
+func (m Message) GenSummary() string {
+	return string(m.Message) + "from" + m.UserInfo.Name + "@" + m.UserInfo.IPAddress
 }
 
 // Message 封装了聊天消息，在系统内部，聊天消息以此形式传播
 type Message struct {
+	SendTime time.Time
+	Message  []byte
+	UserInfo Info
+}
+
+// New generate a message
+func New(message []byte, sendtime time.Time, userinfo Info) Message {
+	return Message{
+		SendTime: sendtime,
+		Message:  message,
+		UserInfo: userinfo,
+	}
 }
 
 // Sendable 抽象了在系统内部可以被传播的内容
